ddb49eef1a3c7e1a306e91026a06c54978e80822
owendswang <9076865+owendswang@users.noreply.github.com>
2021-03-10 11:55:08 +0800

    Change the order when to get the image urls.
    The solution didn't change, only the order of 'add button' and 'get image urls'.

--- Download pictures from telegraph.user.js
+++ Download pictures from telegraph.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Download pictures from telegraph
 // @name:zh-CN   下载Telegraph页面图片
-// @version      0.1
+// @version      0.2
 // @description  Download pictures from telegra.ph
 // @description:zh-CN 下载“telegra.ph”页面上的图片
 // @author       OWENDSWANG
@@ -26,40 +26,37 @@
         })
     }
 
-    var figureList = document.getElementsByTagName('figure');
-    var imgSrcList = [];
-    for (var i = 0; i < figureList.length; i++) {
-        var img = figureList[i].getElementsByTagName('img')[0];
-        var src = img.getAttribute('src');
-        imgSrcList.push(src);
-    }
-
-    if (imgSrcList.length > 0) {
-        var addressList = document.getElementsByTagName('address');
-        for (var j = 0; j < addressList.length; j++) {
-            if (addressList[j].getAttribute("dir") == 'auto') {
-                var button = document.createElement('button');
-                button.innerHTML = 'Download';
-                // button.setAttribute('style', 'margin-left: 10px;');
-                button.style.marginLeft = '10px';
-                button.style.paddingLeft = '15px';
-                button.style.paddingRight = '15px';
-                button.style.backgroundColor = 'rgba(0,0,0,0)';
-                button.style.border = '2px solid rgba(0,0,0,0.5)';
-                button.style.borderRadius = '5px';
-                button.type = 'button';
-                // button.setAttribute('type', 'button');
-                button.addEventListener('mouseover', function(event) {
-                    this.style.backgroundColor = 'rgba(0,0,0,0.1)';
-                });
-                button.addEventListener('mouseout', function(event) {
-                    this.style.backgroundColor = 'rgba(0,0,0,0)';
-                });
-                button.onclick = function(){
-                    download(imgSrcList);
-                };
-                addressList[j].appendChild(button);
-            }
+    var addressList = document.getElementsByTagName('address');
+    for (var j = 0; j < addressList.length; j++) {
+        if (addressList[j].getAttribute("dir") == 'auto') {
+            var button = document.createElement('button');
+            button.innerHTML = 'Download';
+            // button.setAttribute('style', 'margin-left: 10px;');
+            button.style.marginLeft = '10px';
+            button.style.paddingLeft = '15px';
+            button.style.paddingRight = '15px';
+            button.style.backgroundColor = 'rgba(0,0,0,0)';
+            button.style.border = '2px solid rgba(0,0,0,0.5)';
+            button.style.borderRadius = '5px';
+            button.type = 'button';
+            // button.setAttribute('type', 'button');
+            button.addEventListener('mouseover', function(event) {
+                this.style.backgroundColor = 'rgba(0,0,0,0.1)';
+            });
+            button.addEventListener('mouseout', function(event) {
+                this.style.backgroundColor = 'rgba(0,0,0,0)';
+            });
+            button.onclick = function(){
+                var figureList = document.getElementsByTagName('figure');
+                var imgSrcList = [];
+                for (var i = 0; i < figureList.length; i++) {
+                    var img = figureList[i].getElementsByTagName('img')[0];
+                    var src = img.getAttribute('src');
+                    imgSrcList.push(src);
+                }
+                download(imgSrcList);
+            };
+            addressList[j].appendChild(button);
         }
     }
 })();
