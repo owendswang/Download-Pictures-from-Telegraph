fe2189d51b00f7d5ace85bb88d260eb5545fd835
owendswang <9076865+owendswang@users.noreply.github.com>
2021-07-22 15:25:16 +0800

    Update to v0.3: Added 2 functions
    1. Display total count of images in the download button;
    2. Rename images to page title in the order of page display.

--- Download pictures from telegraph.user.js
+++ Download pictures from telegraph.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Download pictures from telegraph
 // @name:zh-CN   下载Telegraph页面图片
-// @version      0.2
+// @version      0.3
 // @description  Download pictures from telegra.ph
 // @description:zh-CN 下载“telegra.ph”页面上的图片
 // @author       OWENDSWANG
@@ -18,11 +18,15 @@
 (function() {
     'use strict';
 
-    function download(imgSrcList) {
+    function download(imgSrcList, imgNameHeader) {
+        var i = 1;
         imgSrcList.forEach(function(src) {
-            var fileName = src.split('/')[src.split('/').length - 1];
-            fileName = fileName.split('?')[0];
+            // var fileName = src.split('/')[src.split('/').length - 1];
+            // fileName = fileName.split('?')[0];
+            var ext = src.split('.')[src.split('.').length - 1];
+            var fileName = imgNameHeader + ' (' + i.toString() + ').' + ext;
             GM_download(src, fileName);
+            i += 1;
         })
     }
 
@@ -30,7 +34,8 @@
     for (var j = 0; j < addressList.length; j++) {
         if (addressList[j].getAttribute("dir") == 'auto') {
             var button = document.createElement('button');
-            button.innerHTML = 'Download';
+            var figureList = document.getElementsByTagName('figure');
+            button.innerHTML = '<b>Download</b> (' + figureList.length.toString() + ' images in total)';
             // button.setAttribute('style', 'margin-left: 10px;');
             button.style.marginLeft = '10px';
             button.style.paddingLeft = '15px';
@@ -47,14 +52,14 @@
                 this.style.backgroundColor = 'rgba(0,0,0,0)';
             });
             button.onclick = function(){
-                var figureList = document.getElementsByTagName('figure');
                 var imgSrcList = [];
+                var pageTitle = document.getElementsByTagName('h1')[0].textContent;
                 for (var i = 0; i < figureList.length; i++) {
                     var img = figureList[i].getElementsByTagName('img')[0];
                     var src = img.getAttribute('src');
                     imgSrcList.push(src);
                 }
-                download(imgSrcList);
+                download(imgSrcList, pageTitle);
             };
             addressList[j].appendChild(button);
         }
