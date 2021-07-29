d496d8fb5dcdc5cee1f3113733cf491d8e64067e
owendswang <9076865+owendswang@users.noreply.github.com>
2021-07-29 17:52:13 +0800

    Update to v0.4: Added another button at bottom
    Added another download button at bottom of the article. Easier to download after seeing through the page.

--- Download pictures from telegraph.user.js
+++ Download pictures from telegraph.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Download pictures from telegraph
 // @name:zh-CN   下载Telegraph页面图片
-// @version      0.3
+// @version      0.4
 // @description  Download pictures from telegra.ph
 // @description:zh-CN 下载“telegra.ph”页面上的图片
 // @author       OWENDSWANG
@@ -33,6 +33,7 @@
     var addressList = document.getElementsByTagName('address');
     for (var j = 0; j < addressList.length; j++) {
         if (addressList[j].getAttribute("dir") == 'auto') {
+            // header button
             var button = document.createElement('button');
             var figureList = document.getElementsByTagName('figure');
             button.innerHTML = '<b>Download</b> (' + figureList.length.toString() + ' images in total)';
@@ -51,7 +52,7 @@
             button.addEventListener('mouseout', function(event) {
                 this.style.backgroundColor = 'rgba(0,0,0,0)';
             });
-            button.onclick = function(){
+            button.onclick = function() {
                 var imgSrcList = [];
                 var pageTitle = document.getElementsByTagName('h1')[0].textContent;
                 for (var i = 0; i < figureList.length; i++) {
@@ -62,6 +63,40 @@
                 download(imgSrcList, pageTitle);
             };
             addressList[j].appendChild(button);
+
+            // bottom button
+            var divButtonBottom = document.createElement('div');
+            divButtonBottom.style.display = 'flex';
+            divButtonBottom.style.justifyContent = 'center';
+            var buttonBottom = document.createElement('button');
+            buttonBottom.innerHTML = '<b>Download</b> (' + figureList.length.toString() + ' images in total)';
+            buttonBottom.style.margin = '10px';
+            buttonBottom.style.paddingTop = '10px';
+            buttonBottom.style.paddingBottom = '10px';
+            buttonBottom.style.paddingLeft = '20px';
+            buttonBottom.style.paddingRight = '20px';
+            buttonBottom.style.backgroundColor = 'rgba(0,0,0,0)';
+            buttonBottom.style.border = '2px solid rgba(0,0,0,0.5)';
+            buttonBottom.style.borderRadius = '5px';
+            buttonBottom.type = 'button';
+            buttonBottom.addEventListener('mouseover', function(event) {
+                this.style.backgroundColor = 'rgba(0,0,0,0.1)';
+            });
+            buttonBottom.addEventListener('mouseout', function(event) {
+                this.style.backgroundColor = 'rgba(0,0,0,0)';
+            });
+            buttonBottom.onclick = function() {
+                var imgSrcList = [];
+                var pageTitle = document.getElementsByTagName('h1')[0].textContent;
+                for (var i = 0; i < figureList.length; i++) {
+                    var img = figureList[i].getElementsByTagName('img')[0];
+                    var src = img.getAttribute('src');
+                    imgSrcList.push(src);
+                }
+                download(imgSrcList, pageTitle);
+            };
+            divButtonBottom.appendChild(buttonBottom);
+            document.getElementById('_tl_editor').appendChild(divButtonBottom);
         }
     }
 })();
